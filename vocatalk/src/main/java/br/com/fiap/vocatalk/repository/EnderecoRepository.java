0415ba513e85e9baa4bafc8039dbc60e8b5060b1
HenriqueCS03 <rm94276@fiap.com.br>
2023-04-30 22:05:45 -0300

    Repositorys

---
+++ vocatalk/src/main/java/br/com/fiap/vocatalk/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.vocatalk.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<EnderecoRepository,Long>{
+    
+}
---
+++ vocatalk/src/main/java/br/com/fiap/vocatalk/repository/ItensFatura.java
@@ -0,0 +1,7 @@
+package br.com.fiap.vocatalk.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItensFatura extends JpaRepository<ItensFatura, Long>{
+    
+}
